53b3583c9c852cde49b43ac4d788c8f07bba3b7b
Takuya Sato <sasuke.tak@gmail.com>
2016-04-14 21:38:22 +0900

    経過

---
+++ src/sdk/workspace/riso-ranger2016/sys/arch/interface/SubsystemBridge.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  SubsystemBridge.cpp
+//  Implementation of the Class SubsystemBridge
+//  Created on:      2016/04/13 12:52:15
+//  Original author: 037789
+///////////////////////////////////////////////////////////
+
+#include "SubsystemBridge.h"
+
+
+SubsystemBridge::SubsystemBridge(){
+
+}
+
+
+
+SubsystemBridge::~SubsystemBridge(){
+
+}
---
+++ src/sdk/workspace/riso-ranger2016/sys/arch/interface/SubsystemIf.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  SubsystemIf.cpp
+//  Implementation of the Class SubsystemIf
+//  Created on:      2016/04/13 12:52:15
+//  Original author: 037789
+///////////////////////////////////////////////////////////
+
+#include "SubsystemIf.h"
+
+
+SubsystemIf::SubsystemIf(){
+
+}
+
+
+
+SubsystemIf::~SubsystemIf(){
+
+}
