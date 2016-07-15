699f28a2e47ebebbaba560247acf7b0721161118
mura_ryu <mura_ryu@riso.co.jp>
2016-07-15 16:11:57 +0900

    WheelControlクラス、WheelMotorクラスのコード自動生成 実装途中

---
+++ src/sdk/workspace/riso-ranger2016/sys/device/WheelControl.cpp
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////
+//  WheelControl.cpp
+//  Implementation of the Class WheelControl
+//  Created on:      2016/07/15 16:04:13
+//  Original author: 039889
+///////////////////////////////////////////////////////////
+
+#include "WheelControl.h"
+
+
+
+
+WheelControl::WheelControl(){
+
+}
+
+
+WheelControl::~WheelControl(){
+
+}
+
+
+void WheelControl::Init(){
+	m_RefSpeed = 0;
+	m_RefTurn = 0;
+	m_WheelMotorR = NULL;
+	m_WheelMotorR = NULL;
+	m_TwoWheelMode = false;
+}
+
+
+// 4ms周期実行 要リアルタイム性
+void WheelControl::Control(){
+
+	if(m_TwoWheelMode == true) {	// 倒立走行
+
+	}
+	else {							// 尻尾走行
+
+	}
+
+}
+
+
+// 同時変更必要
+void WheelControl::SetRefValue(int speed, int turn){
+	m_RefSpeed = speed;
+	m_RefTurn = turn;
+}
+
+
+void WheelControl::SetTwoWheelMode(bool onoff){
+	m_TwoWheelMode = onoff;
+}
+
+
+void WheelControl::SetWheelMotorL(WheelMotor* wheelMotorL) {
+	m_WheelMotorL = wheelMotorL;
+}
+
+
+void WheelControl::SetWheelMotorR(WheelMotor* wheelMotorR) {
+	m_WheelMotorR = wheelMotorR;
+}
---
+++ src/sdk/workspace/riso-ranger2016/sys/device/WheelControl.h
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////
+//  WheelControl.h
+//  Implementation of the Class WheelControl
+//  Created on:      2016/07/15 16:04:13
+//  Original author: 039889
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_AD0BBBF1_1A41_451b_9449_09D1A5901DD9__INCLUDED_)
+#define EA_AD0BBBF1_1A41_451b_9449_09D1A5901DD9__INCLUDED_
+
+#include "WheelMotor.h"
+
+class WheelControl
+{
+
+public:
+	WheelControl();
+	virtual ~WheelControl();
+	void Init();
+	void Control();
+	void SetRefValue(int speed, int turn);
+	void SetTwoWheelMode(bool onoff);
+	void SetWheelMotorL(WheelMotor* wheelMotorL);
+	void SetWheelMotorR(WheelMotor* wheelMotorR);
+
+private:
+	bool m_TwoWheelMode;
+	WheelMotor* m_WheelMotorL;
+	WheelMotor* m_WheelMotorR;
+	int m_RefSpeed;
+	int m_RefTurn;
+
+};
+#endif // !defined(EA_AD0BBBF1_1A41_451b_9449_09D1A5901DD9__INCLUDED_)
