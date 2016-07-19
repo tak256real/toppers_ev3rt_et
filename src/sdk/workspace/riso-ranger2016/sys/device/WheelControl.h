f1a198609ee24714082c99150f449f5b709a2470
mura_ryu <mura_ryu@riso.co.jp>
2016-07-19 21:53:13 +0900

    コメント追加

--- src/sdk/workspace/riso-ranger2016/sys/device/WheelControl.h
+++ src/sdk/workspace/riso-ranger2016/sys/device/WheelControl.h
@@ -24,11 +24,11 @@ public:
 	void SetWheelMotorR(WheelMotor* wheelMotorR);
 
 private:
-	bool m_TwoWheelMode;
-	WheelMotor* m_WheelMotorL;
-	WheelMotor* m_WheelMotorR;
-	int m_RefSpeed;
-	int m_RefTurn;
+	bool m_TwoWheelMode;		// 倒立制御オンオフ
+	WheelMotor* m_WheelMotorL;	// 左モータ
+	WheelMotor* m_WheelMotorR;	// 右モータ
+	int m_RefSpeed;				// 目標速度
+	int m_RefTurn;				// 目標旋回速度
 
 };
 #endif // !defined(EA_AD0BBBF1_1A41_451b_9449_09D1A5901DD9__INCLUDED_)
