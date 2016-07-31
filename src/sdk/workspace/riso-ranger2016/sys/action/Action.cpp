975e42c58db26f6cf5108c3c032573e78ebfc17a
muraryu <s1013129@gmail.com>
2016-08-01 06:22:58 +0900

    メンバ操作まわり変更

--- src/sdk/workspace/riso-ranger2016/sys/action/Action.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/action/Action.cpp
@@ -19,12 +19,36 @@ Action::~Action(){
 
 }
 
-
-void Action::cycle(){
+void Action::EmergencyStop(){
 
 }
 
 
-void Action::EmergencyStop(){
+void Action::setStateObserver(StateObserver* stateObserver){
+	m_StateObserver = stateObserver;
+}
 
+
+StateObserver* Action::getStateObserver(){
+	return m_StateObserver;
+}
+
+
+void Action::setTailControl(TailControl* tailControl){
+	m_TailControl = tailControl;
+}
+
+
+TailControl* Action::getTailControl(){
+	return m_TailControl;
+}
+
+
+void Action::setWheelControl(WheelControl* wheelControl){
+	m_WheelControl = wheelControl;
+}
+
+
+WheelControl* Action::getWheelControl(){
+	return m_WheelControl;
 }
--- src/sdk/workspace/riso-ranger2016/sys/action/Action.h
+++ src/sdk/workspace/riso-ranger2016/sys/action/Action.h
@@ -18,13 +18,21 @@ class Action
 public:
 	Action();
 	virtual ~Action();
+
 	virtual void cycle()=0;
 	void EmergencyStop();
 
-private:
-	StateObserver *m_StateObserver;
-	TailControl *m_TailControl;
-	WheelControl *m_WheelControl;
+	void setStateObserver(StateObserver* stateObserver);
+	StateObserver* getStateObserver();
+	void setTailControl(TailControl* tailControl);
+	TailControl* getTailControl();
+	void setWheelControl(WheelControl* wheelControl);
+	WheelControl* getWheelControl();
+
+protected:
+	StateObserver* m_StateObserver;
+	TailControl* m_TailControl;
+	WheelControl* m_WheelControl;
 
 };
 #endif // !defined(EA_328EF76F_92B6_4a4a_BB28_306BB0AD8654__INCLUDED_)
