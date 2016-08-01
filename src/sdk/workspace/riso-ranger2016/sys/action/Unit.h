26feb544b96771ed1f266b16e19d9f698ec59076
mura_ryu <mura_ryu@riso.co.jp>
2016-08-01 21:31:42 +0900

    Unitクラス追加

---
+++ src/sdk/workspace/riso-ranger2016/sys/action/Unit.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  Unit.h
+//  Implementation of the Class Unit
+//  Created on:      2016/08/01 21:29:44
+//  Original author: 039389
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_FCCC6601_27E9_4f76_A2BC_B46D8060E9D3__INCLUDED_)
+#define EA_FCCC6601_27E9_4f76_A2BC_B46D8060E9D3__INCLUDED_
+
+#include "Action.h"
+
+class Unit
+{
+
+public:
+	Unit();
+	virtual ~Unit();
+	Action *currentAction;
+
+};
+#endif // !defined(EA_FCCC6601_27E9_4f76_A2BC_B46D8060E9D3__INCLUDED_)
