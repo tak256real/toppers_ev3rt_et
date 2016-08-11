ae8722e3ab7e9e61fe705110a801e09a712cc67f
muraryu <s1013129@gmail.com>
2016-08-11 21:20:34 +0900

    ファイル追加し忘れ

---
+++ src/sdk/workspace/riso-ranger2016/sys/device/Bluetooth.h
@@ -0,0 +1,21 @@
+/*
+ * Bluetooth.h
+ *
+ *  Created on: 2016/08/11
+ *      Author: ryuji
+ */
+
+#ifndef WORKSPACE_RISO_RANGER2016_SYS_DEVICE_BLUETOOTH_H_
+#define WORKSPACE_RISO_RANGER2016_SYS_DEVICE_BLUETOOTH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+FILE *btlog = NULL;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* WORKSPACE_RISO_RANGER2016_SYS_DEVICE_BLUETOOTH_H_ */
