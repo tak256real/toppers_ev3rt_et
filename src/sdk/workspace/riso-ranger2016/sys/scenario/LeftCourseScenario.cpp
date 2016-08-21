a606d7d4a92f0fc8f63280dff9c01fca6e3b9d04
ReinaSaito <rsaito@riso.co.jp>
2016-08-21 18:41:38 +0900

    ルックアップゲート部調整済み

--- src/sdk/workspace/riso-ranger2016/sys/scenario/LeftCourseScenario.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/scenario/LeftCourseScenario.cpp
@@ -33,15 +33,18 @@ void LeftCourseScenario::start(){
 	// シーケンス順設定
 	Sequence* sequence;
 	Sequence* firstSequence;
-	firstSequence = 							new Sequence(new SitWaitAction(90),			new TimeCondition(5000)		);
-	sequence = firstSequence->setNextSequence(	new Sequence(new LineTraceAction(10, 60),	new DistanceToObjectCondition(200))		);
-	sequence = sequence->setNextSequence(		new Sequence(new TailBrakeAction(),			new TimeCondition(1000))		);
-	sequence = sequence->setNextSequence(		new Sequence(new RecliningAction(-5),		new InclinationCondition(45))		);
-	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(20, 0),		new DistanceCondition(400))		);
-	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(-20, 0),		new DistanceCondition(-400))		);
-	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(20, 0),		new DistanceCondition(400))		);
-	sequence = sequence->setNextSequence(		new Sequence(new RecliningAction(5),		new InclinationCondition(90))		);
-	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(30, 60),	new DistanceCondition(20))		);
+	firstSequence = 							new Sequence(new SitWaitAction(92),			new TimeCondition(5000)		);
+	sequence = firstSequence->setNextSequence(	new Sequence(new LineTraceAction(10, 60),	new DistanceCondition(200))		);
+//	sequence = sequence->setNextSequence(		new Sequence(new TailBrakeAction(),			new TimeCondition(1000))		);
+	sequence = sequence->setNextSequence(		new Sequence(new RecliningAction(-4),		new InclinationCondition(68))		);
+	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(20, 0),		new DistanceCondition(100))		);
+	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(10, 0),		new DistanceCondition(100))		);
+//	sequence = sequence->setNextSequence(		new Sequence(new TailBrakeAction(),			new TimeCondition(2000))		);
+	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(-10, 0),		new DistanceCondition(-100))		);
+	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(10, 0),		new DistanceCondition(200))		);
+//	sequence = sequence->setNextSequence(		new Sequence(new TailRunAction(20, 0),		new DistanceCondition(400))		);
+//	sequence = sequence->setNextSequence(		new Sequence(new RecliningAction(5),		new InclinationCondition(85))		);
+//	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(30, 60),	new DistanceCondition(20))		);
 
 
 	// シーケンス開始
