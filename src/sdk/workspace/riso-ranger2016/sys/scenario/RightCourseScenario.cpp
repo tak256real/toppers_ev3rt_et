cacb2943d6fe8e16fde8b34ef1149286fb60483b
muraryu <s1013129@gmail.com>
2016-09-17 22:08:47 +0900

    Ⅰ段目登って止まる

--- src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
@@ -50,7 +50,9 @@ void RightCourseScenario::start(){
 
 	// 階段
 	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(50, 45, new PIDControl(80, 0, 3000), 0.5),	new DistanceCondition(200))		);	// スタート直後はゆっくり走行
-	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(80, 80, 0),								new DistanceCondition(10000))		);	// スタート直後はゆっくり走行
+	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(80, 80, 0),								new CollisionCondition())		);	// スタート直後はゆっくり走行
+	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(80, 80, 0),								new DistanceCondition(150))		);	// スタート直後はゆっくり走行
+	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(0, 80, 0),								new EmptyCondition())		);	// スタート直後はゆっくり走行
 
 	sequence = sequence->setNextSequence(	new Sequence(new TailBrakeAction(),											new TimeCondition(10000))		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new TailRunAction(20, 0),										new CollisionCondition())		);	// 座って待機
