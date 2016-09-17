2d41afeb47920f32fc08ef0e7b2a34c366597f6a
muraryu <s1013129@gmail.com>
2016-09-17 22:22:10 +0900

    Ⅰ段目登って尻尾ブレーキで止まるまでの距離短縮

--- src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
@@ -51,7 +51,7 @@ void RightCourseScenario::start(){
 	// 階段
 	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(50, 45, new PIDControl(80, 0, 3000), 0.5),	new DistanceCondition(200))		);	// スタート直後はゆっくり走行
 	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(80, 80, 0),								new CollisionCondition())		);	// スタート直後はゆっくり走行
-	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(80, 80, 0),								new DistanceCondition(150))		);	// スタート直後はゆっくり走行
+	sequence = sequence->setNextSequence(	new Sequence(new DirectionRunAction(80, 80, 0),								new DistanceCondition(50))		);	// スタート直後はゆっくり走行
 	sequence = sequence->setNextSequence(	new Sequence(new TailBrakeAction(),											new EmptyCondition())		);	// スタート直後はゆっくり走行
 
 	sequence = sequence->setNextSequence(	new Sequence(new TailBrakeAction(),											new TimeCondition(10000))		);	// 座って待機
