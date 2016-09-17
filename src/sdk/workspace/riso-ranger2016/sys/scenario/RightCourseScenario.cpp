9dddc234fbc4d02c857b0d8e1b935b7c8ba7a442
muraryu <s1013129@gmail.com>
2016-09-17 20:49:23 +0900

    pid修正

--- src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
@@ -48,10 +48,10 @@ void RightCourseScenario::start(){
 //	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(80, 45, new PIDControl(80, 0, 3000)),	new EmptyCondition())		);	// 直線終わりまで
 
 	// 階段
-	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(50, 80, new PIDControl(40, 0, 0), 0.5),	new DistanceCondition(30000))		);	// スタート直後はゆっくり走行
+	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(50, 80, new PIDControl(80, 0, 3000), 0.5),	new DistanceCondition(30000))		);	// スタート直後はゆっくり走行
 	sequence = sequence->setNextSequence(	new Sequence(new TailBrakeAction(),											new TimeCondition(1000))		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new TailRunAction(20, 0),										new CollisionCondition())		);	// 座って待機
-	sequence = sequence->setNextSequence(	new Sequence(new SitWaitAction(75),											new TimeCondition())		);	// 座って待機
+	sequence = sequence->setNextSequence(	new Sequence(new SitWaitAction(75),											new TimeCondition(1000))		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new TailRunAction(-20, 0),										new DistanceCondition(-200))	);	// 一定距離バック
 	sequence = sequence->setNextSequence(	new Sequence(new SitWaitAction(75),											new TimeCondition(2000))		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new TailRunAction(50, 0),										new DistanceCondition(350))		);	// 勢いをつけて登る
