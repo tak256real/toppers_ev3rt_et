7c4cbaecf9f6bcf49e5a642c03dbb2ad12e626fe
takaki58 <s1170007@gmail.com>
2016-09-17 20:42:06 +0900

    文字化け修正

--- src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
@@ -37,13 +37,13 @@ RightCourseScenario::~RightCourseScenario(){
 
 
 void RightCourseScenario::start(){
-	// ƒV[ƒPƒ“ƒX‡Ý’è
+	// シーケンス順設定
 	Sequence* sequence;
 	Sequence* firstSequence;
 ///////////////////////////////////
-//ƒXƒ^[ƒg
+//スタート
 ///////////////////////////////////
-  // K”öƒLƒƒƒŠƒuƒŒ[ƒVƒ‡ƒ“•”
+  // 尻尾キャリブレーション部
 	firstSequence = 							new Sequence(new TailCalibrationAction(),	new TailStopCondition(1000)	);
 	sequence = firstSequence->setNextSequence(	new Sequence(new SitWaitAction(97),			new CommandCondition())	);
 	// スタート部
@@ -54,27 +54,27 @@ void RightCourseScenario::start(){
 	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(80, 2, 0.5, new PIDControl(80, 0.5, 3000)),	new DistanceCondition(1250)));
 	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(40, 2, 0.5, new PIDControl(80, 0.5, 3000)),	new DistanceCondition(1100)));
 //////////////////////////////////////
-//ƒS[ƒ‹
+//ゴール
 //////////////////////////////////////
 //////////////////////////////////////
-//ŠK’i
+//階段
 //////////////////////////////////////
 	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(50, 2, 0.5, new PIDControl(80, 0.5, 3000)),	new DistanceCondition(720)));
 
 
 //////////////////////////////////////
-//ŠK’iI‚í‚è
+//階段終わり
 //////////////////////////////////////
 ///////////////////////////////////////
-//ƒKƒŒ[ƒWƒVƒiƒŠƒI
+//ガレージ
 ///////////////////////////////////////
-	//ToDo ƒOƒŒ[ƒ‰ƒCƒ“‚ÌŒŸ’m‚·‚éˆÊ’u‚ªŒˆ‚Ü‚Á‚½‚ç”÷’²®
+	//ToDo グレーラインの検知する位置が決まったら微調整
 	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(40, 60, 0.5, new PIDControl(80, 0.5, 3000)),	new DistanceCondition(80))	);
 	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(30, 80, 0.5, new PIDControl(80, 0.5, 3000)),	new DistanceCondition(80))	);
 	sequence = sequence->setNextSequence(		new Sequence(new LineTraceAction(20, 80, 0.5, new PIDControl(80, 0.5, 3000)),	new DistanceCondition(40))	);
 	sequence = sequence->setNextSequence(		new Sequence(new TailBrakeAction(),			new TimeCondition(700))		);
 ///////////////////////////////////////
-//ƒKƒŒ[ƒWƒVƒiƒŠƒII‚í‚è
+//ガレージ終わり
 ///////////////////////////////////////
 
 
