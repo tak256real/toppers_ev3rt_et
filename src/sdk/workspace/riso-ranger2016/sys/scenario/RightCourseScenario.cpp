b3fdac04efde99ad94dd851dbdabb3bbfb4b8d82
muraryu <s1013129@gmail.com>
2016-09-17 20:59:36 +0900

    ぶつかったら尻尾ブレーキ

--- src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
+++ src/sdk/workspace/riso-ranger2016/sys/scenario/RightCourseScenario.cpp
@@ -48,8 +48,8 @@ void RightCourseScenario::start(){
 //	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(80, 45, new PIDControl(80, 0, 3000)),	new EmptyCondition())		);	// 直線終わりまで
 
 	// 階段
-	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(70, 80, new PIDControl(80, 0, 3000), 0.5),	new DistanceCondition(30000))		);	// スタート直後はゆっくり走行
-	sequence = sequence->setNextSequence(	new Sequence(new TailBrakeAction(),											new TimeCondition(1000))		);	// 座って待機
+	sequence = sequence->setNextSequence(	new Sequence(new LineTraceAction(20, 80, new PIDControl(80, 0, 3000), 0.5),	new CollisionCondition())		);	// スタート直後はゆっくり走行
+	sequence = sequence->setNextSequence(	new Sequence(new TailBrakeAction(),											new TimeCondition(10000))		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new TailRunAction(20, 0),										new CollisionCondition())		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new SitWaitAction(75),											new TimeCondition(1000))		);	// 座って待機
 	sequence = sequence->setNextSequence(	new Sequence(new TailRunAction(-20, 0),										new DistanceCondition(-200))	);	// 一定距離バック
